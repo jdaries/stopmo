16fbfa6034d4038b3f69d20fbd1dc2e2e0ae76e1
Jon Daries <daries@mit.edu>
2020-08-10 20:37:38 -0700

    adding button

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from picamera import PiCamera
 from time import sleep
+from gpiozero import Button
 
+button = Button(17)
 camera = PiCamera()
 
-camera.rotation = 180
+#camera.rotation = 180
 camera.start_preview()
-sleep(3)
+button.wait_for_press()
 camera.capture('/home/pi/Documents/stopmo_files/image.jpg')
 camera.stop_preview()
