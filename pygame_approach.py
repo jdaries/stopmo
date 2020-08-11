c80855337a308e65b5d803ea07bd3048c5cb22fc
Jon Daries <daries@mit.edu>
2020-08-11 15:44:44 -0700

    trying some things

--- pygame_approach.py
+++ pygame_approach.py
@@ -12,8 +12,8 @@ PROJECT = "testing"
 button = Button(17)
 
 
-def frame_display_ghost(W, H):
-    last_image = frame_get_last(W, H)
+def frame_display_ghost(W, H, frame_no):
+    last_image = frame_get_last(W, H, frame_no)
     SCREEN.blit(last_image, (0, 0))
     pygame.display.update()
 
@@ -28,27 +28,27 @@ def frame_get_last(W, H, frame_no):
     return image
 
 
-if '__name__' == '__main__':
-    pygame.init()
-    W, H = pygame.display.list_modes()[0]
-    WIDTH = int(W)
-    HEIGHT = int(H)
-    print('Resolution: ({WIDTH}, {HEIGHT}).'.format(WIDTH=WIDTH, HEIGHT=HEIGHT))
-    SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
-    pygame.mouse.set_visible = False
-    pygame.display.toggle_fullscreen()
-    CAMERA = PiCamera()
-    CAMERA.preview_alpha = 200
-    CAMERA.resolution = (WIDTH, HEIGHT)
 
-    frame = 1
+pygame.init()
+W, H = pygame.display.list_modes()[0]
+WIDTH = int(W)
+HEIGHT = int(H)
+print('Resolution: ({WIDTH}, {HEIGHT}).'.format(WIDTH=WIDTH, HEIGHT=HEIGHT))
+SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.mouse.set_visible = False
+pygame.display.toggle_fullscreen()
+CAMERA = PiCamera()
+CAMERA.preview_alpha = 200
+CAMERA.resolution = (WIDTH, HEIGHT)
 
-    while True:
-        if frame > 1:
-            CAMERA.stop_preview()
-            frame_display_ghost(WIDTH, HEIGHT)
+frame = 1
 
-        CAMERA.start_preview(fullscreen=False, window = (W-WIDTH, 100, WIDTH, HEIGHT))
-        button.wait_for_press()
-        camera.capture('{}/{}/frame_{}.jpg'.format(IMG_DIR, PROJECT, str(frame)))
-        frame += 1
+while True:
+    if frame > 1:
+        CAMERA.stop_preview()
+        frame_display_ghost(WIDTH, HEIGHT, frame)
+
+    CAMERA.start_preview(fullscreen=False, window = (W-WIDTH, 100, WIDTH, HEIGHT))
+    button.wait_for_press()
+    camera.capture('{}/{}/frame_{}.jpg'.format(IMG_DIR, PROJECT, str(frame)))
+    frame += 1
