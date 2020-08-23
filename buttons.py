d95cf961b0d6b8d0daf53d81288675bebb49d184
Jon Daries <daries@mit.edu>
2020-08-23 13:06:59 -0700

    adding preview start and stop to button functions

--- buttons.py
+++ buttons.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+import pygame
 
+from pygame.locals import *
 from gpiozero import Button
+from picamera import PiCamera
 
 # 2 = delete movie
 # 3 = preview movie
@@ -9,24 +12,39 @@ from gpiozero import Button
 # 27 - save movie
 # 22 - exit program
 
-delete_movie_button = Button(2)
-preview_movie_button = Button(3)
+stop_preview_button = Button(2)
+preview_button = Button(3)
 delete_frame_button = Button(4)
 take_picture_button = Button(17)
 save_movie_button = Button(27)
 exit_program_button = Button(22)
 
 
+pygame.init()
+W, H = pygame.display.list_modes()[0]
+WIDTH = int(W)
+HEIGHT = int(H)
+print('Resolution: ({WIDTH}, {HEIGHT}).'.format(WIDTH=WIDTH, HEIGHT=HEIGHT))
+SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.mouse.set_visible = False
+pygame.display.toggle_fullscreen()
+CAMERA = PiCamera()
+CAMERA.preview_alpha = 200
+CAMERA.resolution = (WIDTH, HEIGHT)
+
+
 def echo(stmt):
     print(stmt)
 
 
-def delete_button():
-    echo("delete movie button pressed")
+def stop():
+    print("stop preview button pressed")
+    CAMERA.stop_preview()
 
 
-def preview_button():
-    echo("preview button pressed")
+def preview():
+    print("preview button pressed")
+    CAMERA.start_preview()
 
 
 def deleteframe_button():
@@ -46,8 +64,8 @@ def exit_button():
 
 
 while True:
-    delete_movie_button.when_pressed = delete_button
-    preview_movie_button.when_pressed = preview_button
+    stop_preview_button.when_pressed = stop
+    preview_movie_button.when_pressed = preview
     delete_frame_button.when_pressed = deleteframe_button
     take_picture_button.when_pressed = takepic_button
     save_movie_button.when_pressed = save_button
