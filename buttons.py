4ba53280ae8bb4977a1f2958dd85c3c4c313bcd9
Jon Daries <daries@mit.edu>
2020-09-15 15:05:23 -0700

    be able to stop and restart

--- buttons.py
+++ buttons.py
@@ -80,7 +80,7 @@ def stop():
 def preview():
     print("preview button pressed")
     CAMERA.start_preview()
-    if get_next_frame() == '{}/{}/frames/frame_{}.jpg'.format(HOME_DIR, PROJECT, str(1).zfill(PAD_WIDTH)):
+    if get_next_frame(offset=1) == '{}/{}/frames/frame_{}.jpg'.format(HOME_DIR, PROJECT, str(0).zfill(PAD_WIDTH)):
         return
     else:
         ghost_preview()
