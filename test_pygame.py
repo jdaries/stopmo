6b30c25d2be8f993eadb61f6d53a9760edd95143
Jon Daries <daries@mit.edu>
2021-09-30 16:00:33 -0700

    seeing if pygame works

---
+++ test_pygame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+import pygame
+pygame.init()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit(); #sys.exit() if sys is imported
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_0:
+                print("Hey, you pressed the key, '0'!")
+            if event.key == pygame.K_1:
+                print("Doing whatever")
